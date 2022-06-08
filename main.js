eb63a6f6fbacd343c6f332104184e6a9b9392716
renan-meloo <renan.meloo.710@gmail.com>
2022-06-08 09:44:50 -0300

    Section Contact

--- main.js
+++ main.js
@@ -35,3 +35,20 @@ const swiper = new Swiper('.swiper-container', {
   mousewheel: true,
   keyboard: true
 })
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
