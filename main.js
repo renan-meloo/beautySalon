0b2c5b20be646dbf26976a8f2381bc8d66eddac9
renan-meloo <renan.meloo.710@gmail.com>
2022-06-14 15:45:32 -0300

    Footer and Transations

--- main.js
+++ main.js
@@ -21,15 +21,15 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
   if (this.window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
-// Slide testemunhos - Swiper
+// Slide - Swiper
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   mousewheel: true,
@@ -48,7 +48,26 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+// Botão para voltar ao Topo
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if (window.scrollY >= 660) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+// When Scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
